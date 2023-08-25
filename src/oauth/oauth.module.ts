337e63387657abac1e1e8022afffbc719fbe2eee
2paperstar <lee@paperst.ar>
2023-08-25 22:05:20 +0900

    chore: add cache-manager

--- src/oauth/oauth.module.ts
+++ src/oauth/oauth.module.ts
@@ -3,9 +3,10 @@ import { OauthController } from './oauth.controller';
 import { OauthService } from './oauth.service';
 import { ClientModule } from 'src/client/client.module';
 import { UserModule } from 'src/user/user.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [UserModule, ClientModule],
+  imports: [UserModule, ClientModule, CacheModule.register()],
   controllers: [OauthController],
   providers: [OauthService],
 })
