7294d208dfa89398f0d3b67208667283ba6fd6e2
2paperstar <lee@paperst.ar>
2023-08-25 18:32:38 +0900

    feat: getUserByUsernameAndPassword

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  getUserByUsernameAndPassword(username: string, password: string) {
+    const user = this.userRepository.getUserByUsername(username);
+    if (user && user.password === password) {
+      return user;
+    }
+    return null;
+  }
+}
