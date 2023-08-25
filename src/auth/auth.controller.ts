cd7fc56712c8294aa370503a3f56b7971b9bfa64
2paperstar <lee@paperst.ar>
2023-08-25 19:02:39 +0900

    feat: login page

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { UserEntity } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('login')
+  @Render('auth/login')
+  loginPage() {
+    return { sample: this.userService.getRandomUser() };
+  }
+
   @Get('info')
   @Render('auth/info')
-  info() {
+  info(@Res() res: Response) {
+    if (true) {
+      res.status(302).redirect('/auth/login');
+    }
     return { user: UserEntity.randomWithId(0) };
   }
 }
