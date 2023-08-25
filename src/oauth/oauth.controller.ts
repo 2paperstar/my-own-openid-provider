99a3bd136c5f59d1a362af96d1a1e15334452a5d
2paperstar <lee@paperst.ar>
2023-08-25 18:11:37 +0900

    feat: authorize endpoint

--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('oauth')
-export class OauthController {}
+export class OauthController {
+  @Get('authorize')
+  @Render('oauth/authorize')
+  authorize() {
+    return {};
+  }
+}
