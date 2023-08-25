3e171e3715df5ae01fc405451932e565abe89bb9
2paperstar <lee@paperst.ar>
2023-08-25 18:55:46 +0900

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OauthModule } from './oauth/oauth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [OauthModule],
+  imports: [OauthModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
