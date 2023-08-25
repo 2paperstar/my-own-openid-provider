9643830fb8b2c9e7ab9eb905d2d8c6c1a5730c81
2paperstar <lee@paperst.ar>
2023-08-25 18:58:51 +0900

    feat: auth/info

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('info')
+  @Render('auth/info')
+  info() {
+    return { user: UserEntity.randomWithId(0) };
+  }
+}
